124c631a948152300f03f17327bcb41beaeaa0a8
Alexey Sevastynov <alexseva94@gmail.com>
2023-12-10 18:27:31 +0200

    added animation for InfoText & Button AddRecipe

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,11 +2,14 @@ import S from "./header.module.scss";
 
 import { Link } from "react-router-dom";
 import { Btn } from "../Btn/Btn";
-import { useAppDispatch } from "../../redux/hook";
+import { useAppDispatch, useAppSelector } from "../../redux/hook";
 import { setIsEditRecipe, setRecipeNameToEdit } from "../../redux/recipesSlice";
 
 export const Header = () => {
   const dispatch = useAppDispatch();
+  const listProducts = useAppSelector((state) => state.products.listProducts);
+
+  const isNotEmptyList = listProducts.length !== 0;
 
   const createRecipe = () => {
     dispatch(setIsEditRecipe(false));
@@ -16,9 +19,13 @@ export const Header = () => {
     <div className={S.header}>
       <div className="container">
         <div className={S.headerWrapper}>
-          <Link to={"/create-recipe"}>
-            <Btn onClick={createRecipe}>add Recipe</Btn>
-          </Link>
+          {isNotEmptyList ? (
+            <Link to={"/create-recipe"}>
+              <Btn onClick={createRecipe}>add Recipe</Btn>
+            </Link>
+          ) : (
+            <p>Тут зявиться Button, додай продукт</p>
+          )}
         </div>
       </div>
     </div>
